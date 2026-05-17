ada7b136b75a65030acdaed66724f31da9bb973d
Lucad <lucad7@gmail.com>
2026-05-17 22:20:55 +0200

    feat(reports): improve template variable hints

--- frontend/src/components/template-hint.vue
+++ frontend/src/components/template-hint.vue
@@ -7,26 +7,86 @@
     icon="help_outline"
     color="grey-5"
     class="template-hint-btn"
+    @mouseenter="openHint"
+    @mouseleave="scheduleClose"
+    @focus="openHint"
+    @blur="scheduleClose"
+    @click.stop="toggleHint"
   >
-    <q-tooltip
+    <q-menu
+      v-model="isOpen"
+      no-parent-event
       anchor="top middle"
       self="bottom middle"
-      :delay="200"
-      class="template-hint-tooltip"
+      :offset="[0, 6]"
+      @mouseenter="cancelClose"
+      @mouseleave="scheduleClose"
     >
-      <div class="text-caption text-weight-medium q-mb-xs text-grey-4">{{ $t('templateHintLabel') }}</div>
-      <code class="template-hint-code">{{ templateVar }}</code>
-    </q-tooltip>
+      <div class="template-hint-tooltip">
+        <div class="text-caption text-weight-medium q-mb-xs text-grey-4">{{ $t('templateHintLabel') }}</div>
+        <code class="template-hint-code">{{ templateVar }}</code>
+        <div v-if="description" class="template-hint-description q-mt-sm">{{ description }}</div>
+        <template v-if="examples.length">
+          <div class="text-caption text-weight-medium q-mt-sm q-mb-xs text-grey-4">{{ $t('templateHintExamples') }}</div>
+          <code
+            v-for="example in examples"
+            :key="example"
+            class="template-hint-code template-hint-example"
+          >
+            {{ example }}
+          </code>
+        </template>
+      </div>
+    </q-menu>
   </q-btn>
 </template>
 
 <script>
 export default {
   name: 'TemplateHint',
+  data() {
+    return {
+      isOpen: false,
+      closeTimer: null
+    }
+  },
   props: {
     templateVar: {
       type: String,
       required: true
+    },
+    description: {
+      type: String,
+      default: ''
+    },
+    examples: {
+      type: Array,
+      default: () => []
+    }
+  },
+  beforeUnmount() {
+    this.cancelClose()
+  },
+  methods: {
+    openHint() {
+      this.cancelClose()
+      this.isOpen = true
+    },
+    toggleHint() {
+      this.cancelClose()
+      this.isOpen = !this.isOpen
+    },
+    scheduleClose() {
+      this.cancelClose()
+      this.closeTimer = setTimeout(() => {
+        this.isOpen = false
+      }, 180)
+    },
+    cancelClose() {
+      if (this.closeTimer) {
+        clearTimeout(this.closeTimer)
+        this.closeTimer = null
+      }
     }
   }
 }
@@ -47,13 +107,25 @@ export default {
   border 1px solid rgba(255,255,255,0.1)
   border-radius 6px
   padding 8px 12px
+  max-width 520px
+  user-select text
 
 .template-hint-code
+  display block
   font-family monospace
   font-size 0.82rem
   color #5eeadb
   background rgba(255,255,255,0.07)
   padding 2px 6px
   border-radius 4px
-  white-space nowrap
+  white-space pre-wrap
+  overflow-wrap anywhere
+
+.template-hint-description
+  color rgba(255,255,255,0.78)
+  font-size 0.78rem
+  line-height 1.35
+
+.template-hint-example + .template-hint-example
+  margin-top 4px
 </style>
